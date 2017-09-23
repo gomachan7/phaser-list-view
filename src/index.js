f39a46bb9c72c15107c777cbe782b6a1a1b01e26
gomachan_7 <tt@a-azarashi.jp>
2017-09-23 23:55:56 +0900

    Remove unused module

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
 import Scroller from './scroller'
 import ListView from './list_view'
 import ListViewCore from './list_view_core'
-import SwipeCarousel from './swipe_carousel'
-import WheelScroller from './wheel_scroller'
+// import SwipeCarousel from './swipe_carousel'
+// import WheelScroller from './wheel_scroller'
 import DirectionalScroller from './directional_scroller'
-import BasicSwiper from './basic_swiper'
-import ScrollerEventDispatcher from './scroller_event_dispatcher'
+// import BasicSwiper from './basic_swiper'
+// import ScrollerEventDispatcher from './scroller_event_dispatcher'
 
 const PhaserListView = {
   Scroller,
   ListView,
   ListViewCore,
-  SwipeCarousel,
-  WheelScroller,
+  // SwipeCarousel,
+  // WheelScroller,
   DirectionalScroller,
-  BasicSwiper,
-  ScrollerEventDispatcher,
+  // BasicSwiper,
+  // ScrollerEventDispatcher,
 }
 
 export {
   Scroller,
   ListView,
   ListViewCore,
-  SwipeCarousel,
-  WheelScroller,
+  // SwipeCarousel,
+  // WheelScroller,
   DirectionalScroller,
-  BasicSwiper,
-  ScrollerEventDispatcher,
+  // BasicSwiper,
+  // ScrollerEventDispatcher,
 }
 
 // NOTE: we should only attach to the window in a production build
